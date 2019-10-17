16f1b4c08dd39e8cdf6a5269e8f060f8082abab2
Parmesh A. (parma) <parma@cisco.com>
2019-10-18 00:17:04 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,52 @@
+from flask import Flask,render_template,send_from_directory,request,jsonify
+import os
+from flaskErrorHandler import SafeRun
+app = Flask(__name__)
+
+BASE_URL = os.path.abspath(os.path.dirname(__file__))
+
+
+APP_FOLDER = os.path.join(BASE_URL, "static")
+
+@app.route('/')
+def index():
+
+    return render_template('index.html')
+
+
+@app.route('/index1')
+def index1():
+
+    return render_template('index1.html')
+
+
+@app.route('/index2')
+def index2():
+
+    return render_template('index2.html')
+
+
+
+@app.route('/get_title')
+def get_title():
+    print("in function")
+    return 'Telsa Azad'
+
+
+
+@app.route('/invoke_error')
+def invoke_error():
+    a = 1
+    b = 0
+    c = 1/0
+
+    return jsonify({'result':'success'})
+
+@app.route('/client-app/<path:filename>')
+def client_app_folder(filename):
+    return send_from_directory(APP_FOLDER, filename)
+
+SafeRun(app,{'from_addr':'svarnow.notify@gmail.com','to_addr':'parmesh20120@gmail.com','password':'azadAZAD1!'})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
--- setup.py
+++ setup.py
@@ -1,20 +1,22 @@
 import setuptools
+
 with open("README.md", "r") as fh:
     long_description = fh.read()
+
 setuptools.setup(
-     name='flaskErrorHandler',  
-     version='0.1',
-     scripts=['flaskErrorHandler'] ,
-     author="Parmesh Ashwath",
-     author_email="parmesh20120@gmail.com",
-     description="A flask package to handle exception and notify on gmail",
-     long_description=long_description,
-   long_description_content_type="text/markdown",
-     url="https://github.com/parmeshashwath/flaskErrorHandler",
-     packages=setuptools.find_packages(),
-     classifiers=[
-         "Programming Language :: Python :: 3",
-         "License :: OSI Approved :: MIT License",
-         "Operating System :: OS Independent",
-     ],
- )
+    name="flaskErrorHandler",
+    version="0.3",
+    author="Parmesh Ashwath",
+    author_email="parmesh20120@gmail.com",
+    description="A flask package to handle exception and notify on gmail",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/parmeshashwath/flaskErrorHandler",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
